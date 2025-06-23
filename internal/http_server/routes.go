b657648f4b284ac85ae1b14094ec8bbc49fe4ced
Providence <111455427+directlypro@users.noreply.github.com>
2025-06-23 22:48:37 +0200

    ccav

--- internal/http_server/routes.go
+++ internal/http_server/routes.go
@@ -11,7 +11,7 @@ func NewServiceRouter() *mux.Router {
 	r := mux.NewRouter()
 
 	//Route Register
-	//TODO: this implementation will have to change
+	//TODO: this implementation will have to change, findout how to just add routes and handle endpoints inside the functions
 	r.Handle("/", http.RedirectHandler("/", http.StatusFound))
 	r.HandleFunc("/ccav", ccav.HealthCheck).Methods("GET")
 	r.HandleFunc("/ccav/{id}", ccav.FileUploadHandler).Methods("POST")
